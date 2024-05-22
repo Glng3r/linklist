b079da9a9f49051ab77b67f4839fd0be77577946
Glng3r <edero.2004@gmail.com>
2024-05-22 15:35:17 -0400

    Finished Authentication.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,41 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
 import Link from "next/link";
-export default function Header() {
+import LogoutButton from "./buttons/LogoutButton";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLink } from "@fortawesome/free-solid-svg-icons";
+export default async function Header() {
+  const session = await getServerSession(authOptions);
+  console.log(session);
   return (
     <header className="bg-white border-b py-4">
-      <div className="max-w-4xl flex justify-between mx-auto">
-        <div className="flex gap-6 px-8">
-          <Link href={'/'}>LinkList</Link>
+      <div className="max-w-4xl flex justify-between mx-auto px-6">
+        <div className="flex items-center gap-6">
+          <Link href={'/'} className="flex items-center gap-2 text-blue-700">
+            <FontAwesomeIcon icon={faLink} className="text-blue-500" />
+            <span className="font-bold">LinkList</span>
+          </Link>
           <nav className="flex items-center gap-4 text-slate-500 text-sm">
             <Link href={'/about'}>About</Link>
             <Link href={'/pricing'}>Pricing</Link>
             <Link href={'/contact'}>Contact </Link>
           </nav>
         </div>
-        <nav className="flex gap-4 text-sm text-slate-500 items-center">
-          <Link href={'/login'}>Sign In</Link>
-          <Link href={'/register'}>Create Account</Link>
+        <nav className="flex items-center gap-4 text-sm text-slate-500 items-center">
+          {!!session && (
+            <>
+              <Link href={'/account'}>
+                Hello, {session?.user?.name || session?.user?.email}
+              </Link>
+              <LogoutButton />
+            </>
+          )}
+          {!session && (
+            <>
+              <Link href={'/login'}>Sign In</Link>
+              <Link href={'/login'}>Create Account</Link>
+            </>
+          )}
         </nav>
       </div>
 
