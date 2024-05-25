fa44565a43f24d488743e95170cd64b8193b5341
Glng3r <edero.2004@gmail.com>
2024-05-25 14:45:13 -0400

    working on User panel sidebar

--- src/components/buttons/LogoutButton.js
+++ src/components/buttons/LogoutButton.js
@@ -3,13 +3,23 @@ import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { signOut } from "next-auth/react";
 
-export default function LogoutButton() {
+export default function LogoutButton({
+    className ='flex gap-2 items-center border p-2 px-4 shadow',
+    iconLeft=false,
+    iconClasses=''
+    }) {
     return (
         <button
-        className="flex gap-2 items-center border p-2 px-4 shadow"
+        className={className}
             onClick={() => signOut()}>
+            {iconLeft && (
+                <FontAwesomeIcon icon={faRightFromBracket} className={iconClasses} />
+            )}
             <span>Logout</span>
-            <FontAwesomeIcon icon={faRightFromBracket} />
+            {!iconLeft &&(
+                <FontAwesomeIcon icon={faRightFromBracket} className={iconClasses} />
+            )}
+
         </button>
     )
 }
