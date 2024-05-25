99ed3d469deeeb20b5d4ba7262165b7bbc76f5e6
Glng3r <edero.2004@gmail.com>
2024-05-25 14:09:36 -0400

    restructured directory layout

--- src/app/(app)/account/page.js
+++ src/app/(app)/account/page.js
@@ -1,9 +1,10 @@
 //account page
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
 import UsernameForm from "@/components/forms/UsernameForm";
 import { Page } from "@/models/Page";
+import mongoose from "mongoose";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export default async function AccountPage({ searchParams }) {
 
@@ -12,6 +13,7 @@ export default async function AccountPage({ searchParams }) {
   if (!session) { // redirect if not logged in
     redirect('/');
   }
+  mongoose.connect(process.env.MONGO_URI);
   const page = await Page.findOne({ owner: session?.user?.email });
   if(page){
     return(
