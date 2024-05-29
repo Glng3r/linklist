60b99888564ca3719bb97c7b40d986d1fee11569
Glng3r <edero.2004@gmail.com>
2024-05-29 12:57:40 -0400

    finished social media links

--- src/components/forms/PageButtonsForm.js
+++ src/components/forms/PageButtonsForm.js
@@ -1,12 +1,13 @@
 'use client';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import SectionBox from "../layout/SectionBox";
-import { faEnvelope, faMobile, faPlus, faSave } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope, faGripLines, faMobile, faPlus, faSave, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { faDiscord, faFacebook, faGithub, faInstagram, faTelegram, faTiktok, faWhatsapp, faYoutube } from "@fortawesome/free-brands-svg-icons";
 import { useState } from "react";
 import SubmitButton from "../buttons/SubmitButton";
 import { savePageButtons } from "@/actions/pageActions";
 import toast from "react-hot-toast";
+import { ReactSortable } from "react-sortablejs";
 const allButtons = [
   { key: 'email', 'label': 'e-mail', icon: faEnvelope, placeholder: 'test@example.com' },
   { key: 'mobile', 'label': 'mobile', icon: faMobile, placeholder: '123-123-1234' },
@@ -23,35 +24,59 @@ const allButtons = [
 function upperFirst(str) {
   return str.slice(0, 1).toUpperCase() + str.slice(1)
 }
-export default function PageButtonsForm() {
+export default function PageButtonsForm({ user, page }) {
 
-  const [activeButtons, setActiveButtons] = useState([]);
+  const pageSavedButtonKeys = Object.keys(page.buttons);
+  const pageSavedButtonsInfo = pageSavedButtonKeys.map(k => allButtons.find(b => b.key === k));
+  const [activeButtons, setActiveButtons] = useState(pageSavedButtonsInfo);
   function addButtonToProfile(button) {
     setActiveButtons(prevButtons => {
       return [...prevButtons, button];
     })
   }
-  async function saveButtons(formData){
+  //saving buttons to profile
+  async function saveButtons(formData) {
     await savePageButtons(formData);
     toast.success('Settings Saved!')
+  }
+  //for removing saved links on profile
+  function removeButton({ key: keyToRemove }) {
+    setActiveButtons(prevButtons => {
+      return prevButtons
+        .filter(button => button.key !== keyToRemove);
+    })
   }
   const availableButtons = allButtons.filter(b1 => !activeButtons.find(b2 => b1.key === b2.key))
   return (
     <SectionBox>
       <form action={saveButtons}>
         <h2 className="text-2xl font-bold mb-4">Buttons</h2>
-        {activeButtons.map(b => (
-          <div className="mb-4 flex items-center">
-            <div className="w-36 flex h-full text-gray-700 p-2 gap-2 items-center">
-              <FontAwesomeIcon icon={b.icon} />
-              <span>{upperFirst(b.label)}:</span>
+        <ReactSortable
+          list={activeButtons}
+          setList={setActiveButtons}>
+          {activeButtons.map(b => (
+            <div key={b.key} className="mb-4 flex items-center">
+              <div className="w-48 flex h-full text-gray-700 p-2 gap-2 items-center">
+                <FontAwesomeIcon
+                  icon={faGripLines}
+                  className="cursor-pointer text-gray-400" />
+                <FontAwesomeIcon icon={b.icon} />
+                <span>{upperFirst(b.label)}:</span>
+              </div>
+              <input
+                placeholder={b.placeholder}
+                name={b.key}
+                defaultValue={page.buttons[b.key]}
+                type="text" style={{ marginBottom: '0' }} />
+              <button
+                onClick={() => removeButton(b)}
+                type="button"
+                className="py-2 px-4 bg-gray-300 cursor-pointer">
+                <FontAwesomeIcon icon={faTrash} />
+              </button>
             </div>
-            <input
-              placeholder={b.placeholder}
-              name={b.key}
-              type="text" style={{ marginBottom: '0' }} />
-          </div>
-        ))}
+          ))}
+        </ReactSortable>
         <div className="flex flex-wrap gap-2 mt-4 border-y py-4">
           {availableButtons.map(b => (
             <button
